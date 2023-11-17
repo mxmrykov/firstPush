bdece445320f45a181b8de8f178606f2db77f8ab
mksrkv <maximkarykov@gmail.com>
2023-11-17 19:04:59 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
 public class Main {
-
     public static void main(String[] args) {
         Order order = new Order();
         System.out.println(order);
--- src/Order.java
+++ src/Order.java
@@ -2,7 +2,6 @@
 public class Order {
 
     private int count = 0;
-
     public Order() {}
 
     public Order(int count) {
